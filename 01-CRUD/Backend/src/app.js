31cf99d0798b6548dd94167db5ff6d0cc7793fdd
Angel Rios <Roraj77@hotmail.com>
2025-03-05 16:44:25 -0600

    Add cors for connect to the cloud

--- 01-CRUD/Backend/src/app.js
+++ 01-CRUD/Backend/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 import {connectDB} from './config/db.js';
 import userRoutes from './routes/user.routes.js';
@@ -15,6 +16,7 @@ const port = process.env.PORT  || 4000;
 connectDB();
 
 // Middleware
+//app.use(cors());
 app.use(express.json());
 app.use('/users', userRoutes);
 
