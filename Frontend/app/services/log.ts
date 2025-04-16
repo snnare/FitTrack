198bf895d81f4526d8f31f96ed59e0f78c4ff2d3
Angel Rios <RoraJ77@hotmail.com>
2025-04-16 11:41:26 -0600

    logs.ts ya accede a todos los services /logs  del backend

--- Frontend/app/services/log.ts
+++ Frontend/app/services/log.ts
@@ -1,11 +1,41 @@
 import api from './api';
 
+
+// userID es el correo del usuario
 export const crearLog = async (logData: any) => {
-  const res = await api.post('/logs', logData);
-  return res.data;
+  try{
+    const response = await api.post('/logs', logData);
+    return response.data;
+  } catch (error) {
+    console.error('Error creating log:', error);
+    throw  error;
+  }
 };
 
 export const getLogFromUser = async (userId: string) => {
-  const res = await api.get(`/logs/${userId}`);
-  return res.data;
+  try {
+    const res = await api.get(`/logs/${userId}`);
+    return res.data;
+  } catch (error: any) {
+    throw error.response?.data || error;
+  }
+};
+
+
+export const getAllLogs = async () => {
+  try {
+    const res = await api.get('/logs');
+    return res.data;
+  } catch (error: any) {
+    throw error.response?.data || error;
+  }
+};
+
+export const updateLog = async (id: string, logData: any) => {
+  try {
+    const res = await api.put(`/logs/${id}`, logData);
+    return res.data;
+  } catch (error: any) {
+    throw error.response?.data || error;
+  }
 };
