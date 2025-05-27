f85277b97d724cd6cf2861bfb9438ac671761790
Angel Rios <Roraj77@hotmail.com>
2025-05-26 23:18:29 -0600

    ya funciona el apk

--- Frontend/app/services/env.ts
+++ Frontend/app/services/env.ts
@@ -1,9 +1,3 @@
-//export const API_URL = "http://172.17.1.46:3000";
-// 192.168.0.155
-// 192.168.0.151/24
-//192.168.0.151/24
-//10.0.2.2
-//export const API_URL = "http://192.168.0.151:3000";
-// export const API_URL = "http://192.168.0.151:3000";
-export const API_URL = "http://20.49.48.166";
+
+export const API_URL = "http://20.49.48.166:4000";
 export const TOKEN_KEY = "TOKEN-USER-KEY";
