e91937ede468386eaed726ad2ca1d6b1e1ac5849
Angel Rios <Roraj77@hotmail.com>
2025-05-26 20:09:38 -0600

    Dockerizando

--- Frontend/app/services/env.ts
+++ Frontend/app/services/env.ts
@@ -5,5 +5,5 @@
 //10.0.2.2
 //export const API_URL = "http://192.168.0.151:3000";
 // export const API_URL = "http://192.168.0.151:3000";
-export const API_URL = "http://20.49.48.166:3000";
+export const API_URL = "http://20.49.48.166";
 export const TOKEN_KEY = "TOKEN-USER-KEY";
