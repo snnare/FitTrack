e22e221ce27040b2d3f00f0078815c5ccc08d879
Angel Rios <Roraj77@hotmail.com>
2025-05-28 18:55:36 -0600

    arreglando bugs

--- Frontend/app/services/env.ts
+++ Frontend/app/services/env.ts
@@ -1,5 +1,5 @@
 
 
-export const API_URL = "http://10.0.2.2:4000";
-//export const API_URL = "http://20.49.48.166:4000";
+//export const API_URL = "http://10.0.2.2:4000";
+export const API_URL = "http://20.49.48.166:4000";
 export const TOKEN_KEY = "TOKEN-USER-KEY";
